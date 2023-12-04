bd09804352b489a2fd9054d6464589893ee63621
ChantileAnnDelaRosa <116992991+ChantileAnn@users.noreply.github.com>
2023-12-04 22:47:29 +0800

    Delete KickSite_Project directory

--- KickSite_Project/php/config.php
+++
@@ -1,5 +0,0 @@
-<?php 
- 
- $con = mysqli_connect("localhost","root","","karate") or die("Couldn't connect");
-
-?>
--- KickSite_Project/sql/tutorial.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users(
-    Id int PRIMARY KEY AUTO_INCREMENT,
-    Email varchar(200),
-    Username varchar(200),
-    Password varchar(200)
-);
